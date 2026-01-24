7a777b559fd454b7ba474e8a35a04c8cb81d1341
Codingnaveen46 <naveenreddy44112gmail.com>
2026-01-24 16:24:05 +0530

    practised the problem for surface of cuboid

---
+++ codingchallanges.py
@@ -0,0 +1,16 @@
+#surface of  a cuboid
+
+#1 what is the cuboid : cuboid is a 3 dimentional  figure
+
+
+length = int(input("enter a area of a cuboid: "))
+breadth = int(input("enter a breadth of a cuboid: "))
+height = int(input("enter a height of a cuboid: "))
+
+area = 2 * (length * height + length * breadth + breadth * height)
+
+print("the area of the cuboid is ",area)
+
+totalanswer = area
+
+print(totalanswer)
