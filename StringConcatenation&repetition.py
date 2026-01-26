5c840e6c0073aff87e681baa6fc0b56cbdc56512
Codingnaveen46 <naveenreddy44112gmail.com>
2026-01-26 19:55:08 +0530

    learned about string concatination

---
+++ StringConcatenation&repetition.py
@@ -0,0 +1,38 @@
+# Revisiting arithmetic operator
+
+# addition
+s  = 'abc'
+a = 10
+
+print(s+ str(a))
+
+
+s1 = 'abcd'
+s2 = 'efgh'
+
+s3 = s1 + s2
+
+print(s3)
+
+
+# strings in multiplication
+
+s = 'a'
+
+s4 = s * 5
+
+print(s4)
+
+
+
+# float operation
+
+#  Resultant data type
+
+t1 = True + True
+
+print(t1)
+
+
+
+
