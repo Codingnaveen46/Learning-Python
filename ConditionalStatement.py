57779b00a9e7faf07310e947b2a5165a753f2d48
Codingnaveen46 <naveenreddy44112gmail.com>
2026-01-26 20:52:42 +0530

    learned about the conditional statements

---
+++ ConditionalStatement.py
@@ -0,0 +1,77 @@
+
+#Learning about the conditional  statements
+
+# Relational operator is used to compare the data
+
+# < less than
+# <= less than equal
+# > greater then
+# >= greater then equal
+# == equal to
+# != not equal
+
+#
+# print(4 < 5)
+# print(4 <= 5)
+# print(4 > 5)
+# print(4 >= 5)
+# print(4 == 5)
+# print(4 != 5)
+#
+
+
+#  conditional statements
+
+# if < condition> :
+#     statement 1
+#  statement 2
+
+#  else :
+# statement 3
+
+# example check a positive or negative
+
+number1 = int(input("Enter number 1 : "))
+# number2 = int(input("Enter  number 2 : "))
+# number3 = int(input("Enter  number 3: "))
+
+
+# if number1 >= 0:
+#     print("The number is positive")
+# else:
+#     print("The number is negative")
+
+
+
+#  compound conditional statement
+
+# and AND  all are should be true
+# or OR    it is always true expect all false
+# nor NOT
+
+
+
+# if number1 > number2 and number1 > number3:
+#     print("true")
+# else:
+#     print("false")
+#
+
+
+# if number1 > number2 or number1 > number3:
+#     print("True")
+# else:
+#     print("False")
+#
+#
+
+# if number1 > 0 and number1 < 10 :
+#     print("True")
+# else:
+#     print("False")
+
+
+if not number1 == 9:
+    print("True")
+else:
+    print("False")
