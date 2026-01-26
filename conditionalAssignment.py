3393522dc1421796fb5a37dbc10149c05d717949
Codingnaveen46 <naveenreddy44112gmail.com>
2026-01-26 21:09:47 +0530

    solved 2 questions related to conditonal

---
+++ conditionalAssignment.py
@@ -0,0 +1,18 @@
+#check even or odd
+
+number1 = int(input("Enter a number: "))
+
+
+if number1  % 2  == 0:
+    print("Even")
+else:
+    print("Odd")
+
+
+
+Age = int(input("Enter your age: "))
+
+if Age >= 18 :
+    print("You are eligible to vote")
+else:
+    print("You are not eligible to vote")
