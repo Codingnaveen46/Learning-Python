e04a46c190c1454de8370fde05789b62f94841cf
Codingnaveen46 <naveenreddy44112gmail.com>
2026-01-29 21:58:48 +0530

    solved 3 questions related to compound conditions

---
+++ compoundConditions.py
@@ -0,0 +1,32 @@
+
+# challenges
+
+# Age checker for the work
+
+age = int(input("Enter your age: "))
+
+if (age >= 18 and age <= 60):
+    print("you are eligible to work")
+else:
+    print("you are not eligible to work")
+
+
+#valid marks
+
+marks = int(input("Enter marks: "))
+
+if (marks >= 0  and marks <= 100):
+    print("you got a good marks")
+else:
+    print("you got a bad marks")
+
+#Gender checker
+
+Gender = input("Enter Gender :")
+
+if Gender == 'male' or Gender == 'MALE':
+    print('You are male')
+else:
+    print('You are female')
+
+
