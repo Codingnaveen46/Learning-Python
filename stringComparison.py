e9945033c5a58946c599ab29afa1ae4a96fd79ba
Codingnaveen46 <naveenreddy44112gmail.com>
2026-02-01 21:22:39 +0530

    learnd about the strings comprasion

---
+++ stringComparison.py
@@ -0,0 +1,29 @@
+#leraning string comparison
+
+# Relational operators (Comparison)
+
+s1 = 'software'
+s2 = 'Hardware'
+
+s3 = s1 > s2
+print(s3)
+
+s4 = 'python'
+s5 = 'pycharm'
+
+s6 = s4 < s5
+print(s6)
+
+s1 = 'integer'
+s2 = 'integer'
+s3 = s1 == s2
+print(s3)
+
+
+#Short circuit
+
+#if first condition is false it will not check the second condition
+
+#non boolean condition
+
+
